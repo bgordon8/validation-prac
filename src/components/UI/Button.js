19eb3494fab1ce1b1291f48850a777a501d1057f
bgordon8 <gordonbrian16@gmail.com>
2023-01-03 13:28:17 -0700

    update button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,35 +1,17 @@
-import styled from "styled-components";
+import React from "react";
 
-const Button = styled.button`
-  font: inherit;
-  border: 1px solid cornflowerblue;
-  border-radius: 8px;
-  background: #4f4f4f;
-  color: white;
-  padding: 0.25rem 1rem;
-  cursor: pointer;
+import classes from "./Button.module.css";
 
-  &:hover,
-  &:active {
-    background: #777777;
-    border-color: #777777;
-  }
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-// const Button = (props) => {
-//   return (
-//     <button
-//       className={classes.button}
-//       type={props.type || "button"}
-//       onClick={props.onClick}
-//     >
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+  return (
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
